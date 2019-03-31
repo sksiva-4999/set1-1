559f0da89da26ba4509460e75ff34cfbf9c252fc
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 20:23:34 +0530

    Create even_interval.c

---
+++ even_interval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int n,q,i;
+  scanf("%d %d",&n,&q);
+  if((n <= 100000 )&& (q <= 100000))
+  {
+    for(i=n+1;i<q;i++)
+    {
+      if(i%2 == 0)
+      {
+        printf("%d\t",i);
+      }
+    }
+  }
+  return 0;
+ }
