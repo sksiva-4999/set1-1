badece62e34a43f235bc9c297012bda6974a8a9d
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 12:20:35 +0530

    Create leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int year;
+  printf("Enter the year\n");
+  if(year % 4 == 0)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("No");
+  }
+  return 0;
+ }
+    
+   
