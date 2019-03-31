9cf9a5b03dc7a24b6b8ed87b96d060c679af7226
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 11:33:33 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -9,14 +9,11 @@ int main()
     if(c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u')
     {
       printf("given character is vowel");
-    }
     else
-    {
       printf("Given character is consonant");
     }
-  }
+  
   else
-  {
     printf("Invalid Input");
   }
   
