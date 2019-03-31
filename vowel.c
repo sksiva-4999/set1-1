e6fbf0163fa5663b1d346f989c26eded7d252e8f
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 11:32:07 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,7 +3,7 @@ int main()
 {
   char c;
   printf("Enter a character\n");
-  scanf("%c",&c);
+  scanf("%c",c);
   if(c =='A'-'Z' || c=='a'-'z')
   {
     if(c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u')
