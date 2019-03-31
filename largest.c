c48f35916efc54477df02f24f43b60e07a3ee7dc
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 12:14:53 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -4,11 +4,11 @@ int main()
   int num1,num2,num3;
   printf("Enter three numbers\n");
   scanf("%d %d %d",&num1,&num2,&num3);
-  if((num1 > num2) && (num1 > num3)
+  if((num1 > num2) && (num1 > num3))
   {
-    printf("%d",num1)
+    printf("%d",num1);
   }
-  elif((num2 > num1) && (num2 > num3))
+  else if((num2 > num1) && (num2 > num3))
   {
     printf("%d",num2);
   }
