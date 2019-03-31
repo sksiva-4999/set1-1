8e6a1fdbeae4f20effca4b9c946fd1a0170c8a94
Aishvarya-Varadharajan <49087166+Aishvarya-Varadharajan@users.noreply.github.com>
2019-03-31 11:43:49 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,18 +3,21 @@ int main()
 {
   char c;
   printf("Enter a character\n");
-  scanf("%c",c);
-  if(c =='A'-'Z' || c=='a'-'z')
+  scanf("%c",&c);
+  if((c >= 'A' && c <= 'Z' )|| (c >='a' && c <= 'z'))
   {
     if(c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u')
     {
       printf("given character is vowel");
+    }
     else
+    {
       printf("Given character is consonant");
     }
-  
+  }
   else
+  {
     printf("Invalid Input");
   }
-  
+ return 0; 
 }
